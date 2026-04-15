2d635e34f1b074e9c7d96cbf1cad385f9dc429aa
Sanskar Thapa <sanskar1016pro@outlook.com>
2026-04-15 13:57:41 -0700

    attn

--- attn_mechanisms/self_attn.py
+++ attn_mechanisms/self_attn.py
@@ -3,3 +3,18 @@ import torch
 
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
+inputs = torch.tensor(
+    [[0.43, 0.15, 0.89],
+     [0.55, 0.87, 0.66],
+     [0.57, 0.85, 0.64],
+     [0.22, 0.58, 0.33],
+     [0.77, 0.25, 0.10],
+     [0.05, 0.80, 0.55]]
+)
+
+query = inputs[1]
+attn_scores_2 = torch.empty(inputs.shape[0])
+
+for i, x_i in enumerate(inputs):
+    attn_scores_2[i] = torch.dot(x_i, query)
+print(attn_scores_2)
